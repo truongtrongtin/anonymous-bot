9f9c5d48150b7f5f3b5e871287af1e18ccca8565
Tin Truong <truongtrongtin0305@gmail.com>
2023-12-30 20:44:46 +0700

    feat: oauth

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,9 +3,14 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      SLACK_BOT_TOKEN: string;
-      SLACK_SIGNING_SECRET: string;
       ALLOWED_SLACK_CHANNEL_IDS: string;
+      SLACK_SIGNING_SECRET: string;
+      SLACK_CLIENT_ID: string;
+      SLACK_CLIENT_SECRET: string;
+      PROJECT_ID: string;
+      SERVICE_ACCOUNT_EMAIL: string;
+      SERVICE_ACCOUNT_PRIVATE_KEY_BASE64: string;
+      DATABASE_ID: string;
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import slackBolt from '@slack/bolt';
+import { installationRef } from './firestore.js';
 import { showReplyMessageModal } from './listeners/actions/showReplyMessageModal.js';
 import { thumbsDown } from './listeners/actions/thumbsDown.js';
 import { thumbsUp } from './listeners/actions/thumbsUp.js';
 import { updateModalWhenSelectChannel } from './listeners/actions/updateModalWhenSelectChannel.js';
+import { appUninstallled } from './listeners/events/appUninstallled.js';
 import { showNewMessageModalFromGlobalShortcut } from './listeners/shortcuts/showNewMessageModalFromGlobalShortcut.js';
 import { showReplyMessageModalFromMessageShortcut } from './listeners/shortcuts/showReplyMessageModalFromMessageShortcut.js';
 import { postNewMessageFromModal } from './listeners/views/postNewMessageFromModal.js';
@@ -11,12 +13,62 @@ import { ignoreRetry } from './middlewares/ignoreRetry.js';
 const { App, ExpressReceiver } = slackBolt;
 
 const expressReceiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
   processBeforeResponse: true,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.DATABASE_ID,
+  scopes: ['chat:write', 'chat:write.public', 'commands'],
+  installationStore: {
+    storeInstallation: async (installation) => {
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        await installationRef.doc(installation.enterprise.id).set(installation);
+        return;
+      }
+      if (installation.team !== undefined) {
+        await installationRef.doc(installation.team.id).set(installation);
+        return;
+      }
+      throw new Error('Failed saving installation data to installationStore');
+    },
+    // @ts-expect-error not type yet
+    fetchInstallation: async (installQuery) => {
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        const installation = await installationRef
+          .doc(installQuery.enterpriseId)
+          .get();
+        return installation.data();
+      }
+      if (installQuery.teamId !== undefined) {
+        const installation = await installationRef
+          .doc(installQuery.teamId)
+          .get();
+        return installation.data();
+      }
+      throw new Error('Failed fetching installation');
+    },
+    deleteInstallation: async (installQuery) => {
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        await installationRef.doc(installQuery.enterpriseId).delete();
+      }
+      if (installQuery.teamId !== undefined) {
+        await installationRef.doc(installQuery.teamId).delete();
+      }
+      throw new Error('Failed to delete installation');
+    },
+  },
 });
 
 export const slackApp = new App({
-  token: process.env.SLACK_BOT_TOKEN,
   receiver: expressReceiver,
 });
 
@@ -26,6 +78,8 @@ slackApp.action('channels-select-action', updateModalWhenSelectChannel);
 slackApp.action('reply-action', showReplyMessageModal);
 slackApp.action('thumbs-up-action', thumbsUp);
 slackApp.action('thumbs-down-action', thumbsDown);
+
+slackApp.event('app_uninstalled', appUninstallled);
 
 slackApp.shortcut('global_new_message', showNewMessageModalFromGlobalShortcut);
 slackApp.shortcut('message_reply', showReplyMessageModalFromMessageShortcut);
